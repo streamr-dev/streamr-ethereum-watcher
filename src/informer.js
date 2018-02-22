f9d4d833e2fe1b79f8b6db9fd8ee272aa96d66ec
Eric Andrews <eric.andrews@streamr.com>
2018-02-22 17:51:12 +0200

    Informer: inform engine-and-editor of events
    Informer class informs engine-and-editor of deployed/undeployed events
    via a HTTP REST API provided by engine-and-editor.

---
+++ src/informer.js
@@ -0,0 +1,44 @@
+const fetch = require('node-fetch')
+const url = require('url')
+
+class Informer {
+    constructor(streamrUrl, devOpsAccessToken) {
+        if (!streamrUrl) {
+            throw 'No streamUrl given'
+        }
+        if (!devOpsAccessToken) {
+            throw 'No devOpsAccessToken given'
+        }
+        this.streamrUrl = streamrUrl
+        this.devOpsAccessToken = devOpsAccessToken
+        this.logging = false
+    }
+
+    setDeployed(id, body) {
+        return this._doRequest(id, 'setDeployed', body)
+    }
+
+    setUndeployed(id, body) {
+        return this._doRequest(id, 'setUndeployed', body)
+    }
+
+    _doRequest(id, action, body) {
+        const apiUrl = url.resolve(this.streamrUrl, `products/${id}/${action}`)
+
+        if (this.logging) {
+            console.info("POST", apiUrl, body ? '\n' + JSON.stringify(body, null, 4) : '')
+        }
+
+        return fetch(apiUrl, {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {
+                'Accept': 'application/json',
+                'Content-type': 'application/json',
+                'Authorization': `Token ${this.devOpsAccessToken}`
+            }
+        })
+    }
+}
+
+module.exports = Informer
---
+++ test/informer-test.js
@@ -0,0 +1,90 @@
+const assert = require('assert')
+const http = require('http')
+
+const Informer = require('../src/informer')
+
+const TEST_SERVER_PORT = 51843
+
+describe('Informer', () => {
+    let server
+    let informer
+    let requests
+
+    before((done) => {
+        server = http.createServer((request, response) => {
+            let body = "";
+            request.on('data', (chunk) => body += chunk)
+            request.on('end', () => {
+                requests.push({
+                    accessToken: request.headers.authorization,
+                    url: request.url,
+                    method: request.method,
+                    body: JSON.parse(body)
+                })
+            })
+            response.end()
+        })
+        server.listen(TEST_SERVER_PORT, (err) => {
+            if (err) {
+                done(err)
+            }
+            console.info(`Test server listening on port ${TEST_SERVER_PORT}`)
+            done()
+        })
+    })
+
+    after((done) => {
+        server.close(done)
+    })
+
+    beforeEach(() => {
+        requests = []
+        informer = new Informer(`http://127.0.0.1:${TEST_SERVER_PORT}`, 'devops-key')
+    })
+
+    it("setDeployed causes expected POST request", async () => {
+        await informer.setDeployed('product-id', {
+            blockNumber: 0,
+            blockIndex: 0,
+            ownerAddress: '0x0',
+            beneficiaryAddress: '0xF',
+            pricePerSecond: 5,
+            priceCurrency: "EUR",
+            minimumSubscriptionInSeconds: 0
+        })
+
+        assert.equal(requests.length, 1)
+        assert.deepEqual(requests[0], {
+            method: 'POST',
+            url: '/products/product-id/setDeployed',
+            accessToken: 'Token devops-key',
+            body: {
+                blockNumber: 0,
+                blockIndex: 0,
+                ownerAddress: '0x0',
+                beneficiaryAddress: '0xF',
+                pricePerSecond: 5,
+                priceCurrency: "EUR",
+                minimumSubscriptionInSeconds: 0
+            }
+        })
+    })
+
+    it("setUndeployed causes expected POST request", async () => {
+        await informer.setUndeployed('product-id', {
+            blockNumber: 0,
+            blockIndex: 0,
+        })
+
+        assert.equal(requests.length, 1)
+        assert.deepEqual(requests[0], {
+            method: 'POST',
+            url: '/products/product-id/setUndeployed',
+            accessToken: 'Token devops-key',
+            body: {
+                blockNumber: 0,
+                blockIndex: 0
+            }
+        })
+    })
+})
