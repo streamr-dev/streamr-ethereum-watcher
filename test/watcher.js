d56eb2d2c697dbc8a54d8d57443e0593bde3feb8
Juuso Takalainen <juuso.takalainen@streamr.com>
2018-02-22 18:07:16 +0200

    set testrpc gas limit (still broken)
    getting Error: base fee exceeds gas limit even though limit is 5M and estimateGas gives 1.5M...

--- test/watcher.js
+++ test/watcher.js
@@ -2,15 +2,19 @@ const testrpc = require("ethereumjs-testrpc")
 const Web3 = require("web3")
 const web3 = new Web3(testrpc.provider({
     mnemonic: "we make your streams come true",
-    total_accounts: 10
+    total_accounts: 10,
+    gasLimit: 5000000
 }))
-
-const marketplace = require("deploy_marketplace")(web3)
 
 const watcher = require("../src/watcher")
 
 describe("Watcher", () => {
+    let marketplace
+    before(async () => {
+        marketplace = await require("./deploy_marketplace")(web3)
+    })
+
     it("starts successfully", () => {
-        
+
     })
 })
