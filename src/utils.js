9049b7cb4fdbcb37cf90c5308aa66b2e76c23e11
Juuso Takalainen <juuso.takalainen@streamr.com>
2018-03-05 18:00:08 +0200

    Fixed gas estimation problems

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
+const Transaction = require("ethereumjs-tx")
+
+function max(a, b) {
+    return a > b ? a : b
+}
+
 /**
  * Call helper. VERY surprisingly and unlike pre-1.0, web3 doesn't seem to automagically calculate the gas limit
  * @param from address where call is sent from
@@ -5,10 +11,12 @@
  * @returns {Promise<any>} resolves after function call returns with receipt (is in blockchain)
  */
 async function sendFrom(from, tx) {
-    return tx.send({
-        from,
-        gas: await tx.estimateGas()
-    })
+    // send at least baseFee to work around Ganache throwing for gas less than basefee,
+    //   EVEN IF that actual transaction ends up consuming precisely what was the gasEstimate
+    const baseFee = +new Transaction({ data: tx.encodeABI() }).getBaseFee() + 100000 // extra for good measure
+    const gasEstimate = await tx.estimateGas({ from })
+    const gas = max(baseFee, gasEstimate)
+    return tx.send({ from, gas })
 }
 
 module.exports = {
