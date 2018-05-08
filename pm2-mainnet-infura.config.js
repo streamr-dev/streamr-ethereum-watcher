d3d4d2b54dbd65968d41be58fb0f282e86ac36b2
Henri Pihkala <henri.pihkala@streamr.com>
2018-05-08 11:53:46 +0000

    using the "new geth server" for watcher backend
    it's been up for many days now!
    
    pm2 start pm2-mainnet-infura.config.js to use infura again.

---
+++ pm2-mainnet-infura.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "watcher-mainnet",
+    script : "index.js",
+    cwd    : "/home/ubuntu/streamr-ethereum-watcher",
+    restart_delay: 5000,
+    args   : "--networkId=1 --streamrApiURL=http://localhost:8081/streamr-core/api/v1 --devopsKey=9FA3g-mqSe2BijSni-U2SQETbEWsXLSBO0t_OWLBccPw --verbose=2"
+  }]
+}
+
