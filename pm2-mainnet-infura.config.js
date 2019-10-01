eda9f1dc570bdf78a416fb8862680733eeab9de3
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-10-01 13:50:24 +0300

    fix config
    whoops, numeric network id IS ok after all, and networkId number is needed for finding the address. Essentially revert previous.

--- pm2-mainnet-infura.config.js
+++ pm2-mainnet-infura.config.js
@@ -4,7 +4,7 @@ module.exports = {
         script : "index.js",
         cwd    : "/home/ubuntu/streamr-ethereum-watcher",
         restart_delay: 5000,
-        args   : "--devopsKey=INSERT_DEVOPS_KEY_HERE --networkId=homestead --streamrApiURL=https://www.streamr.com/api/v1 --verbose=2"
+        args   : "--devopsKey=INSERT_DEVOPS_KEY_HERE --networkId=1 --streamrApiURL=https://www.streamr.com/api/v1 --verbose=2"
     }]
 }
 
