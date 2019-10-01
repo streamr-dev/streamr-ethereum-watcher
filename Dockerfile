2943188df6073657ee79657c86dac05ffcd2e357
Juuso Takalainen <juuso.takalainen@streamr.com>
2019-10-01 13:45:40 +0300

    fix networkId in confs
    now it refers to ethers.js getDefaultProvider names

--- Dockerfile
+++ Dockerfile
@@ -34,7 +34,7 @@ RUN pip install awscli --upgrade --user && \
 ENV STREAMR_API_URL http://localhost:8081/streamr-core/api/v1
 ENV DEVOPS_KEY devops-user-key
 ENV METRICS false
-ENV NETWORK_ID 1
+ENV NETWORK_ID rinkeby
 ENV MARKETPLACE_ADDRESS 0xA10151D088f6f2705a05d6c83719e99E079A61C1
 
 # Secret Variables
--- pm2-mainnet.config.js
+++ pm2-mainnet.config.js
@@ -4,6 +4,6 @@ module.exports = {
         script : "index.js",
         cwd    : "/home/ubuntu/streamr-ethereum-watcher",
         restart_delay: 5000,
-        args   : "--networkId=1 --ethereumServerURL=ws://94.130.70.249:8557 --streamrApiURL=https://www.streamr.com/api/v1 --devopsKey=INSERT_DEVOPS_KEY_HERE --verbose=2"
+        args   : "--ethereumServerURL=ws://94.130.70.249:8557 --streamrApiURL=https://www.streamr.com/api/v1 --devopsKey=INSERT_DEVOPS_KEY_HERE --verbose=2"
     }]
 }
