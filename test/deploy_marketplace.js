582596e726fdec5330357fa9cf3797c2e523b3f7
Juuso Takalainen <juuso.takalainen@streamr.com>
2020-04-08 15:33:07 +0300

    Fix test

--- test/deploy_marketplace.js
+++ test/deploy_marketplace.js
@@ -2,6 +2,7 @@ const {
     ContractFactory,
 } = require("ethers")
 
+const OldMarketplace = require("../lib/marketplace-contracts/build/contracts/OldMarketplace.json")
 const Marketplace = require("../lib/marketplace-contracts/build/contracts/Marketplace.json")
 const Token = require("../lib/marketplace-contracts/build/contracts/MintableToken.json")
 
@@ -10,9 +11,13 @@ module.exports = async wallet => {
     const token = await tokenDeployer.deploy()
     await token.deployed()
 
+    const oldMarketDeployer = new ContractFactory(OldMarketplace.abi, OldMarketplace.bytecode, wallet)
+    const oldMarketplace = await oldMarketDeployer.deploy(token.address, wallet.address)
+    await oldMarketplace.deployed()
+
     const marketDeployer = new ContractFactory(Marketplace.abi, Marketplace.bytecode, wallet)
-    const marketplace = await marketDeployer.deploy(token.address, wallet.address)
+    const marketplace = await marketDeployer.deploy(token.address, wallet.address, oldMarketplace.address)
     await marketplace.deployed()
 
-    return { token, marketplace }
+    return { token, marketplace, oldMarketplace }
 }
