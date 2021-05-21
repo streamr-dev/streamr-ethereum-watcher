e3800d77d7482af4d4cd9737b61d87506435ddef
Kare Nuorteva <kare.nuorteva@me.com>
2021-05-21 18:13:56 +0300

    fix(watcher): use === in if

--- src/watcher.js
+++ src/watcher.js
@@ -140,7 +140,7 @@ class Watcher extends EventEmitter {
                 }
             } catch (e) {
                 // if it was because streamr backend couldn't find the product for set(Un)Deployed, just keep chugging
-                if (e.code == "ECONNREFUSED") { continue }
+                if (e.code === "ECONNREFUSED") { continue }
                 throw e
             }
         }
