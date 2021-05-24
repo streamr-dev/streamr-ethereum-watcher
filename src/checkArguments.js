4e613befd814843cf255ae3fefc8f48f1bd2398c
Kare Nuorteva <kare.nuorteva@me.com>
2021-05-24 13:53:26 +0300

    style(checkArgs): rename variable

--- src/checkArguments.js
+++ src/checkArguments.js
@@ -1,19 +1,19 @@
 const ethers = require("ethers")
 
-/** @typedef {String} EthereumAddress */
-
 /**
  * Validate Ethereum contract addresses from user input
- * @returns {EthereumAddress} checksum-formatted by ethers.js
+ * @param provider Ethers Provider instance
+ * @param address Ethereum address to validate
+ * @returns {string} ethereum address checksum-formatted by ethers.js
  **/
-async function throwIfNotContract(eth, address) {
+async function throwIfNotContract(provider, address) {
     let addr = null
     try {
         addr = ethers.utils.getAddress(address)
     } catch (e) {
         throw new Error(`Error: Bad Ethereum address ${address}`)
     }
-    if (await eth.getCode(addr) === "0x") {
+    if (await provider.getCode(addr) === "0x") {
         throw new Error(`"Error: No contract at ${address}`)
     }
     return addr
