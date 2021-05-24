7326af206b4335048655b0852cad7b24eb019cfe
Kare Nuorteva <kare.nuorteva@me.com>
2021-05-24 13:41:21 +0300

    style(checkArgs): remove unused functions

--- src/checkArguments.js
+++ src/checkArguments.js
@@ -4,15 +4,6 @@ const ethers = require("ethers")
 
 /**
  * Validate Ethereum contract addresses from user input
- * @returns {Maybe<EthereumAddress>} checksum-formatted by ethers.js, or null if not set
- **/
-async function throwIfSetButNotContract(eth, address, variableDescription) {
-    if (!address) { return }
-    return throwIfNotContract(eth, address, variableDescription)
-}
-
-/**
- * Validate Ethereum contract addresses from user input
  * @returns {EthereumAddress} checksum-formatted by ethers.js
  **/
 async function throwIfNotContract(eth, address, variableDescription) {
@@ -22,16 +13,6 @@ async function throwIfNotContract(eth, address, variableDescription) {
     }
     return addr
 }
-
-/**
- * Validate addresses from user input
- * @returns {Maybe<EthereumAddress>} checksum-formatted by ethers.js, or null if not set
- **/
-function throwIfSetButBadAddress(address, variableDescription) {
-    if (!address) { return null }
-    return throwIfBadAddress(address, variableDescription)
-}
-
 /**
  * Validate addresses from user input
  * @returns {EthereumAddress} checksum-formatted by ethers.js
@@ -44,17 +25,6 @@ function throwIfBadAddress(address, variableDescription) {
     }
 }
 
-function throwIfNotSet(variable, description) {
-    if (typeof variable === "undefined") {
-        throw new Error(`${description || "Error"}: Expected a value!`)
-    }
-    return variable
-}
-
 module.exports = {
     throwIfNotContract,
-    throwIfSetButNotContract,
-    throwIfBadAddress,
-    throwIfSetButBadAddress,
-    throwIfNotSet,
 }
