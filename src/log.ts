e80ecf91a009963b50b08b2edfc81760c33444d2
Kare Nuorteva <kare.nuorteva@me.com>
2022-02-22 19:27:01 +0200

    style: disable lint nits from log

--- src/log.ts
+++ src/log.ts
@@ -1,16 +1,20 @@
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 function error(...messages: any[]): void {
     console.error(...messages) // eslint-disable-line no-console
 }
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 function warn(...messages: any[]): void {
     console.warn(...messages) // eslint-disable-line no-console
 }
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 function info(...messages: any[]): void {
     console.info(...messages) // eslint-disable-line no-console
 }
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 function debug(...messages: any[]): void {
     console.debug(...messages) // eslint-disable-line no-console
 }
